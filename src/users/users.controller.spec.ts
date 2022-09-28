f3d48e036be62aa63a26c019c1db3e6d81f974e5
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-28 00:17:20 -0300

    wip: escreve setup de testes

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,17 +4,31 @@ import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
+    // jest.spyOn(UsersService, 'create').mockImplementation(() => {});
     expect(controller).toBeDefined();
   });
 });
