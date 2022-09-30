c98cc6b5cebf2dd582361e2ca6b32ff7989be521
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-09-30 13:05:30 -0300

    test: testa busca por id

--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -3,7 +3,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 import { Users } from '../users.interface';
 import { UsersService } from '../users.service';
-import { createUserDto, mockUsersList, mockUserUpdatedPhone } from './mocks';
+import {
+  createUserDto,
+  mockUser,
+  mockUserId,
+  mockUsersList,
+  mockUserUpdatedPhone,
+} from './mocks';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -21,6 +27,11 @@ describe('UsersService', () => {
               exec: jest.fn().mockResolvedValueOnce(mockUsersList),
             }),
             findOne: jest.fn(),
+            findById: jest.fn().mockReturnValue({
+              select: jest.fn().mockReturnValue({
+                exec: jest.fn().mockResolvedValueOnce(mockUser),
+              }),
+            }),
             update: jest.fn().mockResolvedValue(mockUserUpdatedPhone),
             remove: jest.fn(),
           },
@@ -53,5 +64,10 @@ describe('UsersService', () => {
   it('should list all users', async () => {
     const findAllUsers = await service.findAll();
     expect(findAllUsers).toEqual(mockUsersList);
+  });
+
+  it('should list a specific user by id', async () => {
+    const findUser = await service.findOne(mockUserId);
+    expect(findUser).toEqual(mockUser);
   });
 });
