af2618105b812285169455bcbe0a2815a207d741
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-10-02 23:16:53 -0300

    refactor: trata possibilidade do usuário não existir no update ou no findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   ConflictException,
   Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -33,18 +34,26 @@ export class UsersService {
   }
 
   async findOne(id: string) {
-    if (mongoose.Types.ObjectId.isValid(id))
-      return this.usersModel.findById(id).select('-password').exec();
-    throw new BadRequestException('Invalid Id');
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new BadRequestException('Invalid Id');
+
+    const user = await this.usersModel.findById(id).select('-password').exec();
+    if (!user) throw new NotFoundException();
+
+    return user;
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    if (mongoose.Types.ObjectId.isValid(id))
-      return this.usersModel
-        .findByIdAndUpdate({ _id: id }, updateUserDto, { new: true })
-        .select('-password')
-        .exec();
-    throw new BadRequestException('Invalid Id');
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new BadRequestException('Invalid Id');
+
+    const user = await this.usersModel.findById(id).select('-password').exec();
+    if (!user) throw new NotFoundException();
+
+    return this.usersModel
+      .findByIdAndUpdate({ _id: id }, updateUserDto, { new: true })
+      .select('-password')
+      .exec();
   }
 
   async remove(id: string) {
